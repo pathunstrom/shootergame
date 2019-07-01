da5424ee6546f169dbe1c20f8da27ebae57589c2
Piper Thunstrom <pathunstrom@gmail.com>
2019-06-30 20:28:24 -0400

    Adds PowerUp system.

---
+++ shooter/systems/powerups.py
@@ -0,0 +1,21 @@
+from random import randint
+
+from ppb.systems import System
+
+from shooter.sprites import gameplay
+
+__all__ = [
+    "PowerUp"
+]
+
+
+class PowerUp(System):
+    next_powerup = 15
+    count = 0
+
+    def on_enemy_killed(self, killed, signal):
+        self.count += 1
+        if self.count >= self.next_powerup:
+            killed.scene.add(gameplay.PowerUp(position=killed.enemy.position))
+            self.count = 0
+            self.next_powerup += randint(-3, +3)
